8e3a712f9bc89225b84416c8caeb262c900a4229
Wiktor Kowalski <wiktor9196667@gmail.com>
2020-03-14 21:24:29 +0100

    zad1

---
+++ lista3/zadanie1/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace zadanie1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(14, 14);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(209, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(14, 41);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(209, 20);
+            this.dateTimePicker2.TabIndex = 1;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(14, 68);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(210, 23);
+            this.buttonCalculate.TabIndex = 2;
+            this.buttonCalculate.Text = "Calculate diffrence";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Diffrence is ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 126);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ lista3/zadanie1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace zadanie1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            var date1 = this.dateTimePicker1.Value;
+            var date2 = this.dateTimePicker2.Value;
+            TimeSpan dateDiffrence;
+            if (date1.CompareTo(date2) >= 0) dateDiffrence = date1.Subtract(date2);
+            else dateDiffrence = date2.Subtract(date1);
+            this.label1.Text = "Diffrence is " +
+            $"{dateDiffrence.Days} day" + (dateDiffrence.Days == 1 ? ", " : "s, ") +
+            $"{dateDiffrence.Hours} hour" + (dateDiffrence.Hours == 1 ? ", " : "s, ") +
+            $"{dateDiffrence.Seconds} second" + (dateDiffrence.Seconds == 1 ? "" : "s");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.dateTimePicker1.Format = DateTimePickerFormat.Long;
+        }
+    }
+}
