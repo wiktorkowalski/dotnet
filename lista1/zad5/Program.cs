feb37a710ff48e5a4fc0cb1623f0c292cc0ed1cf
Mikołaj Kalwa <13047857+mikolajkalwa@users.noreply.github.com>
2020-03-19 16:04:09 +0100

    Lista 1, zadanie 5 (#3)
    * Lista 1, zadanie 5
    
    * pull requests suggestions implemented

--- lista1/zad5/Program.cs
+++ lista1/zad5/Program.cs
@@ -1,24 +1,28 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 
 namespace zad5
 {
-    // https://stackoverflow.com/questions/18362368/loading-dlls-at-runtime-in-c-sharp
-    // todo
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string path = @"C:\Users\Wiktor\source\repos\lista1\QuickMafs\bin\Debug\netcoreapp3.1\";
-            string dllname = Console.ReadLine();
-            var DLL = Assembly.LoadFile(path + dllname);
-
-            var theType = DLL.GetType("DLL.Class1");
-            var c = Activator.CreateInstance(theType);
-            var method = theType.GetMethod("Output");
-            method.Invoke(c, new object[] { @"Hello" });
-
-            Console.ReadLine();
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\QuickMafs\bin\Debug\netcoreapp3.1\QuickMafs.dll");
+            string fullPath = Path.GetFullPath(path);
+            var dll = Assembly.LoadFile(fullPath);
+            var type = dll.GetType("QuickMafs.Class1");
+            var instance = Activator.CreateInstance(type);
+            MethodInfo[] methods = type.GetMethods();
+            Console.WriteLine("Lista dostępnych metod:");
+            for (int i = 0; i < methods.Length; i++)
+            {
+                Console.WriteLine($"{i} {methods[i].Name}");
+            }
+            Console.WriteLine("Podaj numer metody którą chcesz wywołać");
+            int methodIndex = Convert.ToInt32(Console.ReadLine());
+            var methodToCall = methods[methodIndex];
+            Console.WriteLine($"Wynik dla parametrów 4,2: {methodToCall.Invoke(instance, new object[] { 4, 2 })}");
         }
     }
 }
