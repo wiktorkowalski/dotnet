91c526e9b3383801f789f174bf455728169de1c6
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2020-03-19 15:51:27 +0100

    Lista 1, zadanie 5

--- lista1/zad5/Program.cs
+++ lista1/zad5/Program.cs
@@ -1,24 +1,38 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 
 namespace zad5
 {
-    // https://stackoverflow.com/questions/18362368/loading-dlls-at-runtime-in-c-sharp
-    // todo
     class Program
     {
         static void Main(string[] args)
         {
-            string path = @"C:\Users\Wiktor\source\repos\lista1\QuickMafs\bin\Debug\netcoreapp3.1\";
-            string dllname = Console.ReadLine();
-            var DLL = Assembly.LoadFile(path + dllname);
-
-            var theType = DLL.GetType("DLL.Class1");
-            var c = Activator.CreateInstance(theType);
-            var method = theType.GetMethod("Output");
-            method.Invoke(c, new object[] { @"Hello" });
-
-            Console.ReadLine();
+            // C:\Users\Mikolaj\Documents\dotnet\lista1\zad5\bin\Debug\netcoreapp3.1\lista1\ClassLibrary1\bin\Debug\ClassLibrary1.dll'    
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\..\QuickMafs\bin\Debug\netcoreapp3.1\QuickMafs.dll");
+            string fullPath = Path.GetFullPath(path);
+            Console.WriteLine(fullPath);
+            var dll = Assembly.LoadFile(fullPath);
+            var type = dll.GetType("QuickMafs.Class1");
+            var instance = Activator.CreateInstance(type);
+            MethodInfo[] methods = type.GetMethods();
+            Console.WriteLine("Lista dostępnych metod:");
+            for (int i = 0; i < methods.Length; i++)
+            {
+                Console.WriteLine($"{i} {methods[i].Name}");
+            }
+            Console.WriteLine("Podaj numer metody którą chcesz wywołać");
+            int methodIndex = Convert.ToInt32(Console.ReadLine());
+            var methodToCall = methods[methodIndex];
+            // w sumie to wszystkie te metody i tak mają tylko po 2 parametry int XD
+            // TODO: brać jakoś dynamicznie parametry od użytkownika
+            //ParameterInfo[] parameters = methodToCall.GetParameters();
+            //Console.WriteLine("Parametry wybranej metody");
+            //foreach (var parameter in parameters)
+            //{
+            //    Console.WriteLine($"{parameter.ParameterType} {parameter.Name}");
+            //}
+            Console.WriteLine($"Wynik dla parametrów 4,2: {methodToCall.Invoke(instance, new object[] { 4, 2 })}");
         }
     }
 }
