271984646933e2b86d25cac7099471576c7dba79
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2020-03-24 13:15:09 +0100

    Lista 2, zadanie 6

---
+++ lista2/zadanie6/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace zadanie6
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Person(int id, string name, string surname, string address, DateTime birthDate)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Address = address;
+            BirthDate = birthDate;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}. {Name} {Surname} {Address} {BirthDate}";
+        }
+    }
+}
