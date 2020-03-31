476e0a1eabe50efb6f3f2c4280b5db7fdfe4879e
Wiktor Kowalski <wiktor9196667@gmail.com>
2020-03-31 17:47:19 +0200

    zad7

---
+++ lista2/zadanie7/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace zadanie7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.codecList = new System.Windows.Forms.ListBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // codecList
+            // 
+            this.codecList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.codecList.FormattingEnabled = true;
+            this.codecList.Location = new System.Drawing.Point(13, 13);
+            this.codecList.Name = "codecList";
+            this.codecList.Size = new System.Drawing.Size(160, 420);
+            this.codecList.TabIndex = 0;
+            this.codecList.SelectedIndexChanged += new System.EventHandler(this.codecList_SelectedIndexChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(180, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(179, 420);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.codecList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox codecList;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ lista2/zadanie7/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zadanie7
+{
+    public partial class Form1 : Form
+    {
+        List<Codec> codecsList;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            codecsList = new List<Codec>();
+            List<ImageCodecInfo> temp = new List<ImageCodecInfo>(ImageCodecInfo.GetImageEncoders());
+            temp.AddRange(ImageCodecInfo.GetImageDecoders());
+            foreach (var item in temp) codecsList.Add(new Codec(item));
+            this.codecList.BeginUpdate();
+            foreach (var item in temp)
+            {
+                this.codecList.Items.Add(new Codec(item)); 
+            }
+            this.codecList.EndUpdate();
+        }
+
+        private void codecList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.richTextBox1.Text = codecsList[this.codecList.SelectedIndex].GetCodecInfo();
+        }
+    }
+}
