0a6d215086bc87cfea55530e2a8a45f7104b92e0
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2020-04-13 19:15:45 +0200

    Zadanie 7

---
+++ lista3/zadanie7/People.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace zadanie7
+{
+    public class People : IEnumerable
+    {
+        private readonly Person[] _people;
+
+        public People(Person[] personArray)
+        {
+            _people = new Person[personArray.Length];
+            personArray.CopyTo(_people, 0);
+        }
+
+        public IEnumerator GetEnumerator()
+
+        {
+            return _people.TakeWhile(person => person != null).GetEnumerator();
+        }
+    }
+}
---
+++ lista3/zadanie7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace zadanie7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] peopleArray =
+            {
+                new Person("Richard", "Brown"),
+                new Person("Howard", "Gagnon"),
+                new Person("Anthony", "Bulluck"),
+                new Person("John", "Bukowski"),
+                new Person("Katia", "Adams"),
+            };
+
+            var peopleList = new People(peopleArray);
+
+            foreach (Person person in peopleList)
+            {
+                Console.WriteLine($"{person.firstName} {person.lastName}");
+            }
+        }
+    }
+}
