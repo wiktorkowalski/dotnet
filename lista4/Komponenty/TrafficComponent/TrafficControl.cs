62b68cd94e7959d8e05597ef87abd4ca20eeb372
Wiktor Kowalski <wiktor9196667@gmail.com>
2020-05-06 18:34:41 +0200

    zad1

---
+++ lista4/Komponenty/TrafficComponent/TrafficControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrafficComponent
+{
+    public partial class TrafficControl : UserControl
+    {
+        private const int alphaOff = 100, alphaOn = 255;
+        public TrafficControl()
+        {
+            InitializeComponent();
+            this.panelRed.BackColor = Color.FromArgb(alphaOff, Color.Red);
+            this.panelYellow.BackColor = Color.FromArgb(alphaOff, Color.Yellow);
+            this.panelGreen.BackColor = Color.FromArgb(alphaOff, Color.Green);
+        }
+
+        public void ChangeState(TrafficState state)
+        {
+            this.panelRed.BackColor = Color.FromArgb(state.HasFlag(TrafficState.Stop) ? alphaOn : alphaOff, Color.Red);
+            this.panelYellow.BackColor = Color.FromArgb(state.HasFlag(TrafficState.Warn) ? alphaOn : alphaOff, Color.Yellow);
+            this.panelGreen.BackColor = Color.FromArgb(state.HasFlag(TrafficState.Go) ? alphaOn : alphaOff, Color.Green);
+        }
+    }
+
+
+    public enum TrafficState
+    {
+        None = 0,
+        Stop = 1,
+        Warn = 2,
+        Go = 4
+    }
+}
---
+++ lista4/Komponenty/zadanie1/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace zadanie1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trafficControl1 = new TrafficComponent.TrafficControl();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // trafficControl1
+            // 
+            this.trafficControl1.Location = new System.Drawing.Point(13, 13);
+            this.trafficControl1.Name = "trafficControl1";
+            this.trafficControl1.Size = new System.Drawing.Size(108, 324);
+            this.trafficControl1.TabIndex = 0;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(128, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(46, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Red";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(128, 36);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(57, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Yellow";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(128, 59);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(55, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Green";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 450);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.trafficControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TrafficComponent.TrafficControl trafficControl1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+    }
+}
+
