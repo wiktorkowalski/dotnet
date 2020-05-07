c018272a19baae7ef0bbad723e66bcdfc70e02b3
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2020-05-07 16:17:12 +0200

    xml zadanie 1

---
+++ lista4/XML/zadanie1/Gender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zadanie1
+{
+    public enum Gender
+    {
+        MALE,
+        FEMALE,
+        APACHE,
+        HELICOPTER,
+        NONBINARY
+    }
+}
---
+++ lista4/XML/zadanie1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace zadanie1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person
+            {
+                Name = "Jan",
+                Surname = "Kowalski",
+                Gender = Gender.HELICOPTER,
+                BirthDate = new DateTime(1999, 12, 6)
+            };
+            var xmlFormat = new XmlSerializer(typeof(Person));
+
+            using Stream fStream = new FileStream("Osoba.xml", FileMode.Create, FileAccess.Write, FileShare.None);
+            xmlFormat.Serialize(fStream, person);
+        }
+    }
+}
