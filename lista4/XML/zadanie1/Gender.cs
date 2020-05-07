f5f62efe0dd6fff888d71f67b72813fff2c8fa8f
Mikołaj Kalwa <13047857+mikolajkalwa@users.noreply.github.com>
2020-05-07 22:49:55 +0200

    Lista4 xml, zadania 1 i 2  (#17)
    * xml zadanie 1
    
    * make person class serializable
    
    * zadanie 2

---
+++ lista4/XML/zadanie1/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zadanie1
+{
+    public enum Gender
+    {
+        MALE,
+        FEMALE,
+        APACHE_HELICOPTER,
+        NONBINARY
+    }
+}
---
+++ lista4/XML/zadanie1/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace zadanie1
+{
+    [Serializable]
+    [XmlRoot(ElementName = "Osoba")]
+    public class Person
+    {
+        [XmlElement(ElementName = "Imie")]
+        public string Name { get; set; }
+        
+        [XmlElement(ElementName = "Nazwisko")]
+        public string Surname { get; set; }
+
+        [XmlElement(ElementName = "Plec")]
+        public Gender Gender { get; set; }
+
+        [XmlElement(ElementName = "DataUrodzenia")]
+        public DateTime BirthDate { get; set; }
+    }
+}
