8aeb02343698b1540a9557deda3ee2f143bd4891
Mikolaj Kalwa <mikolajkalwa@gmail.com>
2020-05-20 14:45:09 +0200

    zadanie 5 WIP

---
+++ lista4/XML/zadanie5/People.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace zadanie5
+{
+    [Serializable]
+    public class People
+    {
+        [XmlArray("Osoby"), XmlArrayItem(typeof(Person), ElementName = "Osoba")]
+        public List<Person> PeopleList { get; set; }
+    }
+}
---
+++ lista4/XML/zadanie5/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace zadanie5
+{
+
+    [Serializable]
+    public class Person
+    {
+        public int Id { get; set; }
+
+        [XmlAttribute(AttributeName = "Imie")]
+        public string Name { get; set; }
+
+        [XmlAttribute(AttributeName = "Nazwisko")]
+        public string Surname { get; set; }
+
+        [XmlAttribute(AttributeName = "Adres")]
+        public string Address { get; set; }
+
+        [XmlAttribute(AttributeName = "NumerTelefonu")]
+        public string PhoneNumber { get; set; }
+
+        public Person(int id, string name, string surname, string address, string phoneNumber)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Address = address;
+            PhoneNumber = phoneNumber;
+        }
+
+        public Person() { }
+
+        public override string ToString()
+        {
+            return $"{Id}. {Name} {Surname} {Address} {PhoneNumber}";
+        }
+    }
+}
