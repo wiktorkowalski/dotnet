356de571cb8fba0ae77dfc012925adf7507d36cc
Wiktor Kowalski <wiktor9196667@gmail.com>
2020-06-16 01:04:08 +0200

    lista6

---
+++ lista6/Zadanie2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadanie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+
+            var query = context.Osoby.Select(item => item).Where(x => x.Imie.Contains("test")).ToList();
+
+            query.ForEach(item => Console.WriteLine(item));
+        }
+    }
+}
---
+++ lista6/Zadanie3/Osoba.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Zadanie3
+{
+    class Osoba
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [NotNull]
+        public string Imie { get; set; }
+
+        [NotNull]
+        public string Nazwisko { get; set; }
+
+        [NotNull]
+        public int Wiek { get; set; }
+
+        public override string ToString() => $"{Id}. {Imie} {Nazwisko} {Wiek}";
+    }
+}
