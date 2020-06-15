6f3c070540600a5535ee5893cca47bbe9c08c421
Wiktor Kowalski <wiktor9196667@gmail.com>
2020-06-16 01:25:11 +0200

    lista6 (#24)

---
+++ lista6/Zadanie1/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zadanie1
+{
+    class Context : DbContext
+    {
+
+        public DbSet<Osoba> Osoby { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseNpgsql("Host=localhost;Database=dotnet;Username=postgres;Password=postgres;Persist Security Info=True");
+
+        }
+    }
+}
+
---
+++ lista6/Zadanie1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+
+            context.Database.EnsureCreated();
+
+            context.Osoby.Add(new Osoba
+            {
+                Imie = "test1",
+                Nazwisko = "test2",
+                Wiek = 1
+            });
+
+            context.SaveChanges();
+
+            Console.WriteLine(context.Osoby.First().ToString());
+        }
+    }
+}
