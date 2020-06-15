6e10eb40c3de4298b3a51cd11458a66783f4c4c8
Wiktor Kowalski <wiktor9196667@gmail.com>
2020-06-16 00:59:17 +0200

    lista5

---
+++ lista5/Zadanie4/Program.fs
@@ -0,0 +1,44 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+type Quaterion(x1: float, y1: float, z1: float, w1: float) =
+
+    let mutable x = x1
+    let mutable y = y1
+    let mutable z = z1
+    let mutable w = w1
+
+    member this.Dot (q: Quaterion) : double = 
+        x * q.X + y * q.X + z * q.X + w * q.X
+
+    member this.Norm (q: Quaterion) : float = 
+        Math.Sqrt(this.Dot(q))
+
+    member this.Scale(scale: float, q: Quaterion) : bool =
+        if scale < 1.0 then
+            x <- x * scale
+            y <- y * scale
+            z <- z * scale
+            w <- w * scale
+            true
+        else false
+
+    member this.MulWith (q: Quaterion) : bool =
+        this.X <- w * q.X + x * q.W + y * q.Z - z * q.Y
+        this.Y <- w * q.Y + y * q.W + z * q.X - x * q.Z
+        this.W <- w * q.W - x * q.X - y * q.Y - z * q.Z
+        this.Z <- w * q.Z + z * q.W + x * q.Y - y * q.X
+        true
+
+    member this.X with get() = x and set(value) = x <- value
+    member this.Y with get() = y and set(value) = y <- value
+    member this.Z with get() = z and set(value) = z <- value
+    member this.W with get() = w and set(value) = w <- value
+
+    
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
---
+++ lista5/Zadanie6/Program.fs
@@ -0,0 +1,50 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+type TokenType =
+    | Operation = 0
+    | Float     = 1
+    
+type Token = {
+    Type: TokenType
+    Value: string
+}
+
+[<Literal>]
+let Operations = "+-*/%"
+[<Literal>]
+let Numbers = "1234567890."
+
+let private isOperation (c: char) = Operations.Contains(c)
+
+let private isNumber (c: char) = Numbers.Contains(c)
+
+let rec private getfloatLenght (input:string) lenght =
+    match input.Length with
+    | 0 -> lenght
+    | _ ->
+        match input.[0] with
+        | c when isNumber c -> getfloatLenght (input.Substring(1)) (lenght + 1)
+        | _ -> lenght
+
+let rec tokenize (input:string) =
+    match input.Length with
+    | 0 -> []
+    | _ ->
+        match input.[0] with
+        | c when isOperation c ->
+            let token = { Type = TokenType.Operation; Value = input.[0].ToString() }
+            token :: (tokenize (input.Substring(1)))
+        | c when isNumber c ->
+            let floatLenght = getfloatLenght input 0
+            let token = { Type = TokenType.Float; Value = input.Substring(0, floatLenght) }
+            token :: (tokenize (input.Substring(floatLenght)))
+        | _ -> tokenize (input.Substring(1))
+
+[<EntryPoint>]
+let main argv =
+    let var = tokenize("5 - 2 * 4 + 6 / 3")
+    for item in var do
+        Console.WriteLine(item.Value.ToString() + " => " + item.Type.ToString());
+    0
